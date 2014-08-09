1a6d09e9fa93ce541a1003f3528d70b8d28f58a9
Jian Li <lijian.gnu@gmail.com>
2014-08-09 14:52:25 +0800

    重写ppwry.py程序

--- tools/IP/QQWry/qqwry.py
+++ tools/IP/QQWry/qqwry.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-# coding: utf-8
+# coding: UTF-8
 
-'''用Python脚本查询纯真IP库
+# 解析 QQWry 库
 
-QQWry.Dat的格式如下:
+'''Python查 询纯真IP库
+
+qqwry.dat 的格式如下:
 
 +----------+
 |  文件头  |  (8字节)
@@ -25,154 +27,404 @@ QQWry.Dat的格式如下:
 
    对于地区记录，可以有两种表示方式： 字符串形式和重定向
 
-   最后一条规则：重定向模式1的国家记录后不能跟地区记录
+   我们称IP记录的第5字节，以及所有重定向后的信息的第1字节，为 falg，依
+   据 flag 值，我们有判定规则：
+
+      1. 0x01 时，＂国家记录/地区信息＂都为 offset 指向的新地址
+      2. 0x02 时，仅＂国家记录＂为 offset 指向的新地址，＂地区信息＂在4字节后
+      3. 0x01, 0x02的情况下，重定向后的信息（新或旧）的首字节如果为 ：
+         - 0，表示无记录（也读不到字符串的）
+         - 0x02，其后3字节整数值为新的 offset 地址
 
 索引区： 每条索引记录格式 ==> 4字节起始IP地址 + 3字节指向IP记录的偏移值
 
    索引区的IP和它指向的记录区一条记录中的IP构成一个IP范围。查询信息是这个
    范围内IP的信息
 
+术语：
+
+每条IP记录的信息有＂国家＂和＂地区＂两部分，但＂国家＂不一定表示＂国
+家＂．我们仅使用这两名词表示信息位置．
+
+纯真IP的问题：
+
+1. 地区信息中，如果最后一位字符值是 '\x96'，可能表示"?"．因为QQWry的
+   Windows查询程序就是这样显示的．目前(2014年8月5日纯真IP库)一共有7组
+   IP段是这样．详情见 decode_str 函数．
+
+   目前发现有这个问题的IP段有：
+
+   1.5.0.0         日本, 港区?
+   120.138.128.0   日本 愛知県名古屋市?
+   133.129.0.0     日本 株式会社大林組?
+   133.131.0.0     日本 ?
+   133.142.0.0     日本 株式会社三菱?
+   133.196.0.0     日本 株式会社?
+   133.255.0.0     日本 ?
+
+2. 记录中其有70处记录中含"?"字符，原因可能是gbk编码表示不了这样字符，纯
+   真IP库生成时就有这个问题（也有可能是用户提交原因）.举例：
+
+     218.93.194.50   218.93.194.50 江苏省宿迁市沭阳县,??街娱乐中心
+   221.199.154.100 221.199.154.100 内蒙古赤峰市,赤峰学院机场路?虎网吧
+    221.203.111.12  221.203.111.12 辽宁省本溪市平山区,佰?网络世界)
+    221.224.55.234  221.224.55.234 江苏省苏州市,浪淘沙连锁网吧鑫轮?直店
+     221.233.1.137   221.233.1.137 湖北省荆州市,??网吧
+   221.236.163.158 221.236.163.158 四川省内江市,?木镇顺发网吧
+     221.239.1.203   221.239.1.203 天津市,依?网吧
+   222.170.192.213 222.170.192.213 黑龙江省双鸭山市宝清县,新?网吧
+     222.175.52.10   222.175.52.10 山东省枣庄市,薛城区?伟网苑
+     222.203.146.0 222.203.147.255 广东省广州市天河区,长?小学
+
+本命令行程序用法示例：
+
+查询多个IP信息：
+
+    python qqwry.py -q 91.121.92.177 88.198.17.4 101.1.25.145
+
+查询 1.list, 2.list 文件中的 IP 信息：
+
+    python qqwry.py --query-file 1.list 2.list
+
+输出所有IP信息到文件 /tmp/ip.txt：
+
+    python qqwry.py --dump -o /tmp/ip.txt
+
+
+最后：
+
+不应完全依赖IP数据库的查询信息，原因有：
+
+1. IP数据库的信息收集有可能不准确
+2. 为了隐藏真实信息，有些IP信息提供方会使用混淆技术
+
 '''
 
+import os
 import sys
 import socket
+import codecs
 from struct import pack, unpack
 
-class IPInfo(object):
-    '''QQWry.Dat数据库查询功能集合
+
+def decode_str(old):
+    '''专门对纯真的gbk编码字符串解压
+
+    返回 utf8 字符串
     '''
-    def __init__(self, dbname):
-        ''' 初始化类，读取数据库内容为一个字符串，
-        通过开始8字节确定数据库的索引信息'''
+    try:
+        return unicode(old,'gbk').encode('utf-8')
+    except:
+        # TODO: hack
+        # 当字符串解码失败，并且最一个字节值为'\x96',则去掉它，再解析
+        if old[-1] == '\x96':
+            try:
+                return unicode(old[:-1],'gbk').encode('utf-8') + '?'
+            except:
+                pass
 
-        self.dbname = dbname
-        f = file(dbname, 'r')
-        self.img = f.read()
-        f.close()
+        return 'Invalid'
 
-        # QQWry.Dat文件的开始8字节是索引信息,前4字节是开始索引的偏移值，
-        # 后4字节是结束索引的偏移值。
-        (self.firstIndex, self.lastIndex) = unpack('II', self.img[:8])
-        # 每条索引长7字节，这里得到索引总个数
-        self.indexCount = (self.lastIndex - self.firstIndex) / 7 + 1
 
-    def getString(self, offset = 0):
-        ''' 读取字符串信息，包括"国家"信息和"地区"信息
+class QQWry(object):
 
-        QQWry.Dat的记录区每条信息都是一个以'\0'结尾的字符串'''
+    def __init__(self, path):
+        self.path = path
+        self.db = None
+        self.open_db()
+        self.idx_start, self.idx_end = self._read_idx()
 
-        o2 = self.img.find('\0', offset)
-        #return self.img[offset:o2]
-        # 有可能只有国家信息没有地区信息，
-        gb2312_str = self.img[offset:o2]
-        try:
-            utf8_str = unicode(gb2312_str,'gb2312').encode('utf-8')
-        except:
-            return '未知'
-        return utf8_str
+    def open_db(self):
+        if not self.db:
+            self.db = open(self.path, 'rb')
+        return self.db
 
-    def getLong3(self, offset = 0):
-        '''QQWry.Dat中的偏移记录都是3字节，本函数取得3字节的偏移量的常规表示
-        QQWry.Dat使用“字符串“存储这些值'''
-        s = self.img[offset: offset + 3]
-        s += '\0'
-        # unpack用一个'I'作为format，后面的字符串必须是4字节
-        return unpack('I', s)[0]
+    def _read_idx(self):
+        '''读取数据库中IP索引起始和结束偏移值
 
-    def getAreaAddr(self, offset = 0):
-        ''' 通过给出偏移值，取得区域信息字符串，'''
+        '''
 
-        byte = ord(self.img[offset])
-        if byte == 1 or byte == 2:
-            # 第一个字节为1或者2时，取得2-4字节作为一个偏移量调用自己
-            p = self.getLong3(offset + 1)
-            return self.getAreaAddr(p)
+        self.db.seek(0)
+        start = unpack('I', self.db.read(4))[0]
+        end = unpack('I', self.db.read(4))[0]
+
+        return start, end
+
+    def read_ip(self, off, seek=True):
+        '''读取ip值（4字节整数值）
+
+        返回IP值
+        '''
+
+        if seek:
+            self.db.seek(off)
+
+        buf = self.db.read(4)
+        return unpack('I', buf)[0]
+
+    def read_offset(self, off, seek=True):
+        '''读取３字节的偏移量值
+
+        返回偏移量的整数值
+        '''
+        if seek:
+            self.db.seek(off)
+
+        buf = self.db.read(3)
+        return unpack('I', buf+'\0')[0]
+
+    def read_string(self, offset):
+        '''读取原始字符串（以"\0"结束）
+
+        返回元组：字符串
+        '''
+
+        if offset == 0:
+            return 'N/A1'
+
+        flag = self.get_flag(offset)
+
+        if flag == 0:
+            # TODO: 出错
+            return 'N/A2'
+
+        elif flag == 2:
+            # 0x02 表示该处信息还是需要重定向
+            offset = self.read_offset(offset+1)
+            return self.read_string(offset)
+
+        self.db.seek(offset)
+
+        raw_string  = ''
+        while True:
+            x = self.db.read(1)
+            if x == '\0':
+                break
+            raw_string += x
+
+        return raw_string
+
+    def get_flag(self, offset):
+        '''读取偏移处的1字节整数值
+
+        QQWry地址信息字符串的第一个字节值可能会是一个标志位，
+        这是一个通用的函数．
+        '''
+        self.db.seek(offset)
+        c = self.db.read(1)
+        if not c:
+            return 0
+        return ord(c)
+
+    def read_record(self, offset):
+
+        self.db.seek(offset)
+
+        # 读取 flag
+        flag = ord(self.db.read(1))
+
+        if flag == 1:
+            # 0x01 表示记录区记录（国家，地区）信息都重定向
+            # 注意：一次重定向后记录还有可能是一个重定向(其flag=0x02)
+
+            buf = self.db.read(3)
+            a_offset = unpack('I', buf+'\0')[0]
+
+            a_raw = self.read_string(a_offset)
+
+            # TODO: hack
+            # 判断新记录的flag是否为0x02，如果是，则表明：
+            # - 国家信息重定向另外地址
+            # - 地区信息为新记录起始地址偏移4字节
+            a_flag = self.get_flag(a_offset)
+            if a_flag == 2:
+                b_raw = self.read_string(a_offset+4)
+            else:
+                b_raw = self.read_string(a_offset+len(a_raw)+1)
+
+        elif flag == 2:
+            # 0x02 表示仅国家记录重定向
+            # 地区信息偏移4字节
+
+            buf = self.db.read(3)
+            a_offset = unpack('I', buf+'\0')[0]
+
+            a_raw = self.read_string(a_offset)
+            b_raw = self.read_string(offset+4)
+
         else:
-            return self.getString(offset)
+            # 正常的信息记录
+            a_raw = self.read_string(offset)
+            b_raw = self.read_string(offset+len(a_raw)+1)
 
-    def getAddr(self, offset, ip = 0):
-        img = self.img
-        o = offset
-        byte = ord(img[o])
+        return a_raw, b_raw
 
-        if byte == 1:
-            # 重定向模式1
-            # [IP][0x01][国家和地区信息的绝对偏移地址]
-            # 使用接下来的3字节作为偏移量调用字节取得信息
-            return self.getAddr(self.getLong3(o + 1))
+    def output(self, output_file='ip.txt'):
+        '''输出所有IP信息到文件
 
-        if byte == 2:
-            # 重定向模式2
-            # [IP][0x02][国家信息的绝对偏移][地区信息字符串]
-            # 使用国家信息偏移量调用自己取得字符串信息
-            cArea = self.getAreaAddr(self.getLong3(o + 1))
-            o += 4
-            # 跳过前4字节取字符串作为地区信息
-            aArea = self.getAreaAddr(o)
-            return (cArea, aArea)
+        '''
 
-        if byte != 1 and byte != 2:
-            # 最简单的IP记录形式，[IP][国家信息][地区信息]
-            # 重定向模式1有种情况就是偏移量指向包含国家和地区信息两个字符串
-            # 即偏移量指向的第一个字节不是1或2,就使用这里的分支
-            # 简单地说：取连续取两个字符串！
+        fp = codecs.open(output_file, 'w', 'utf8')
 
-            cArea = self.getString(o)
-            #o += len(cArea) + 1
-            # 我们已经修改cArea为utf-8字符编码了，len取得的长度会有变，
-            # 用下面方法得到offset
-            o = self.img.find('\0',o) + 1
-            aArea = self.getString(o)
-            return (cArea, aArea)
+        idx = self.idx_start
+        while idx <= self.idx_end:
+
+            ip_int = self.read_ip(idx)
+            ip_start = socket.inet_ntoa(pack('!I', ip_int))
+
+            ip_end_offset = self.read_offset(idx + 4)
+
+            ip_int = self.read_ip(ip_end_offset)
+            ip_end = socket.inet_ntoa(pack('!I', ip_int))
+
+            a_raw, b_raw = self.read_record(ip_end_offset+4)
+
+            a_info = decode_str(a_raw)
+            b_info = decode_str(b_raw)
+
+            fp.write(u'%15s\t%15s\t%s,%s\n' %(
+                ip_start, ip_end,
+                a_info.decode('utf8'), b_info.decode('utf8')))
+
+            # 步进7字节：4字节的起始IP值 + 3字节的结束IP偏移值
+            idx += 7
+
+        fp.close()
 
     def find(self, ip, l, r):
-        ''' 使用二分法查找网络字节编码的IP地址的索引记录'''
+        '''使用二分法查找网络字节编码的IP地址的索引记录
+
+        '''
+
         if r - l <= 1:
             return l
 
         m = (l + r) / 2
-        o = self.firstIndex + m * 7
-        new_ip = unpack('I', self.img[o: o+4])[0]
-        if ip <= new_ip:
+        offset = self.idx_start + m * 7
+
+        new_ip = self.read_ip(offset)
+
+        if ip < new_ip:
             return self.find(ip, l, m)
         else:
             return self.find(ip, m, r)
 
-    def getIPAddr(self, ip):
-        ''' 调用其他函数，取得信息！'''
+    def query(self, ip):
+        '''查询IP信息
+
+        '''
+
         # 使用网络字节编码IP地址
         ip = unpack('!I', socket.inet_aton(ip))[0]
+        # IP索引总数，二分法需要
+        total = (self.idx_end - self.idx_start) / 7 + 1
         # 使用 self.find 函数查找ip的索引偏移
-        i = self.find(ip, 0, self.indexCount - 1)
+        i = self.find(ip, 0, total - 1)
         # 得到索引记录
-        o = self.firstIndex + i * 7
+        o = self.idx_start + i * 7
         # 索引记录格式是： 前4字节IP信息+3字节指向IP记录信息的偏移量
         # 这里就是使用后3字节作为偏移量得到其常规表示（QQWry.Dat用字符串表示值）
-        o2 = self.getLong3(o + 4)
+        o2 = self.read_offset(o + 4)
         # IP记录偏移值+4可以丢弃前4字节的IP地址信息。
-        (c, a) = self.getAddr(o2 + 4)
-        return (c, a)
+        (c, a) = self.read_record(o2 + 4)
+        return (decode_str(c), decode_str(a))
 
-    def output(self, first, last):
-        for i in range(first, last):
-            o = self.firstIndex +  i * 7
-            ip = socket.inet_ntoa(pack('!I', unpack('I', self.img[o:o+4])[0]))
-            offset = self.getLong3(o + 4)
-            (c, a) = self.getAddr(offset + 4)
-            print "%s %d %s/%s" % (ip, offset, c, a)
+    def __del__(self):
+        if self.db:
+            self.db.close()
+
+
+def parse_cmd_args():
+    '''处理命令行选项
+
+    '''
+
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        prog='qqwry',
+        description='qqwry is an IP address lookup process, for QQWry(cz88.net).')
+
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument("-q", dest='query', action="store",
+                       nargs='+', # 可指定多个IP
+                       help="lookup ip.")
+
+    group.add_argument("--query-file", action="store",
+                       dest='query_file',
+                       nargs='+', # 可指定多个IP
+                       help="lookup ip from files")
+
+    group.add_argument("--dump", action="store_true",
+                       help="dump all ip information to a file.")
+
+    parser.add_argument('-f', '--dbpath',
+                        action="store",
+                        default='qqwry.dat',
+                        help='the path of qqwry.dat')
+
+    parser.add_argument('-o', '--output',
+                        action="store",
+                        default='ip.txt',
+                        help='save output of ip info to a file')
+
+
+    args = parser.parse_args()
+    if not (args.query or args.query_file or args.dump):
+        parser.print_help()
+        sys.exit(0)
+
+    return args
 
 
 def main():
-    i = IPInfo('QQWry.Dat')
-    (c, a) = i.getIPAddr(sys.argv[1])
-    print '%s %s/%s' % (sys.argv[1], c, a)
+
+    args = parse_cmd_args()
+
+    # 测试 qqwry.dat 文件
+    if not os.path.exists(args.dbpath):
+        print '%s does not exist, use -f to set it.' % args.dbpath
+        sys.exit(1)
+
+    qqwry = QQWry(args.dbpath)
+
+    if args.query:
+        for ip in args.query:
+            c, a = qqwry.query(ip)
+            print '%15s %s%s' % (ip, c, a)
+
+    elif args.query_file:
+        for p in args.query_file:
+            with open(p, 'r') as f:
+                while True:
+                    ip = f.readline().strip()
+                    if not ip:
+                        break
+
+                    c, a = qqwry.query(ip)
+                    print '%15s %s%s' % (ip, c, a)
+
+    elif args.dump:
+        print 'dumping to %s ...' % args.output
+        qqwry.output()
+
 
 if __name__ == '__main__':
+
     main()
+
+
 
 # changelog
 # 时间：2009年5月29日
 # 1. 工具下面网友的建议，修改"o += len(cArea) + 1"
 #    http://linuxtoy.org/archives/python-ip.html#comment-113960
 #    因为这个时候我已经把得到的字符串变成utf-8编码了，长度会有变化！
+#
+# 时间：2014年8月9日 重写程序
+# 1. 实现完整查询纯真IP库.
+# 2. 可完整dump出所有ip记录，并与纯真官方的查询程序比对正确.
 
+# contact
+# https://ylinux.org
